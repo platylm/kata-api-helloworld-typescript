d4fe6c43cfcd5a806bb62884110b65007cf668e4
platylm <n_nareenart@kkumail.com>
2020-01-14 09:48:39 +0700

    [Created]: index for compile hello world

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express, { Application as expressApplication } from 'express';
+const app = express();
+const port = 3000;
+
+
+// app.get('/hello', (request, response) => {
+//     //response.send('hello world')
+//     response.json({ 'hello': 'world' })
+// })
+
+// app.listen(port, () => {
+//     console.log(`app listening on port ${port}`)
+// })
+
+class Application {
+    public app: expressApplication // from import type express
+    public port: number
+
+    constructor(port: number) { //assign value
+        this.app = express();
+        this.port = port;
+
+        //route -> fn
+        this.app.get('/hello', (request, response) => {
+            //response.send('hello world')
+            response.json({ 'hello': 'world' })
+        })
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log(`app listening on port ${port}`)
+        })
+    }
+}
